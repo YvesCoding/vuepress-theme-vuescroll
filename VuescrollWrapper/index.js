75e05dc4ff36971644dc0729177dee930a7d7d32
wangyi7099 <724003548@qq.com>
2018-09-09 13:55:05 +0800

    change to es5

--- VuescrollWrapper/index.js
+++ VuescrollWrapper/index.js
@@ -1,8 +1,20 @@
-import vuescroll from 'vuescroll';
-import vuescrollMix from './vuescroll-mix';
-import store from './store';
+"use strict";
 
-let _ops = {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.registry = registry;
+exports.default = void 0;
+
+var _vuescroll = _interopRequireDefault(require("vuescroll"));
+
+var _vuescrollMix = _interopRequireDefault(require("./vuescroll-mix"));
+
+var _store = _interopRequireDefault(require("./store"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _ops = {
   bar: {
     background: '#3eaf7c'
   },
@@ -13,29 +25,109 @@ let _ops = {
     scrollingX: false
   }
 };
+var _default = _ops;
+exports.default = _default;
 
-export default _ops;
-
-export function registry({ Vue, router }, ops = _ops) {
-  Vue.use(vuescroll);
-  Vue.mixin(vuescrollMix);
-
-  router.afterEach((to, from) => {
+function registry(_ref) {
+  var Vue = _ref.Vue,
+      router = _ref.router;
+  var ops = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : _ops;
+  Vue.use(_vuescroll.default);
+  Vue.mixin(_vuescrollMix.default);
+  router.afterEach(function (to, from) {
     if (!Vue.prototype.$isServer) {
-      if (window.vs && !store.disableScrollBehavior) {
-        let y;
+      if (window.vs && !_store.default.disableScrollBehavior) {
+        var y;
+
         if (!to.hash) {
           y = 0;
         } else {
           y = document.querySelector(to.hash).offsetTop;
         }
+
         window.vs.scrollTo({
-          y
+          y: y
         });
-        store.specifyAuthor = to.hash;
+        _store.default.specifyAuthor = to.hash;
       }
     }
   });
-
   _ops = ops;
 }
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+// It is not yet time to use Vuex to manage the global state
+// singleton object as a global store.
+var state = {
+  disableScrollBehavior: false
+};
+var _default = state;
+exports.default = _default;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _store = _interopRequireDefault(require("./store"));
+
+var _lodash = _interopRequireDefault(require("lodash.throttle"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = {
+  methods: {
+    handleVueScroll: (0, _lodash.default)(function (scrollTop) {
+      this._setActiveHash(scrollTop);
+    }, 300),
+    _setActiveHash: function _setActiveHash(scrollTop) {
+      var _this = this;
+
+      if (_store.default.specifyAuthor) {
+        _store.default.disableScrollBehavior = true;
+        this.$router.replace(decodeURIComponent(_store.default.specifyAuthor), function () {
+          // execute after scrollBehavior handler.
+          _this.$nextTick(function () {
+            _store.default.disableScrollBehavior = false;
+          });
+        }, function () {
+          // execute after scrollBehavior handler.
+          _this.$nextTick(function () {
+            _store.default.disableScrollBehavior = false;
+          });
+        });
+        return;
+      }
+
+      var sidebarLinks = [].slice.call(document.querySelectorAll('.sidebar-link'));
+      var anchors = [].slice.call(document.querySelectorAll('.header-anchor')).filter(function (anchor) {
+        return sidebarLinks.some(function (sidebarLink) {
+          return sidebarLink.hash === anchor.hash;
+        });
+      });
+
+      for (var i = 0; i < anchors.length; i++) {
+        var anchor = anchors[i];
+        var nextAnchor = anchors[i + 1];
+        var isActive = i === 0 && scrollTop === 0 || scrollTop >= anchor.parentElement.offsetTop + 10 && (!nextAnchor || scrollTop < nextAnchor.parentElement.offsetTop - 10);
+
+        if (isActive && decodeURIComponent(this.$route.hash) !== decodeURIComponent(anchor.hash)) {
+          _store.default.disableScrollBehavior = true;
+          this.$router.replace(decodeURIComponent(anchor.hash), function () {
+            // execute after scrollBehavior handler.
+            _this.$nextTick(function () {
+              _store.default.disableScrollBehavior = false;
+            });
+          });
+          return;
+        }
+      }
+    }
+  }
+};
+exports.default = _default;
